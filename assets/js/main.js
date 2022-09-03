3059a210335395e5a0600f09f37ae0634d30c4d9
Abddeali Kachwala <abdeali@vcaretechnologies.net>
2022-09-03 22:01:02 +0530

    Test Data

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// SVG file to SVG code convert JS Start
+function img2svg() {
+    jQuery('.in__svg').each(function (i, e) {
+        var $img = jQuery(e);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+        jQuery.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', ' ' + imgClass + ' replaced-svg');
+            }
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+        }, 'xml');
+    });
+}
+img2svg();
+// SVG file to SVG code convert JS End
+
+// Menu JS
+$(document).ready(function(){
+    $(".mobile__menu").click(function(){
+      $(this).toggleClass("show");
+      $(".sidebar").toggleClass("show");
+    });
+});
+// Menu JS
